f565289fd76cece5bb62aa03f7b4a84e0d9710e3
lixin <q1404433837@163.com>
2020-04-08 13:29:32 +0800

    [style] 更改ajax封装函数格式 使用起来更加简便

--- ajaxMy.js
+++ ajaxMy.js
@@ -2,11 +2,10 @@ var url_head = "http://192.168.0.180:8081/api"; //http 本地
 //var url_head = "http://127.0.0.1:8888/api"; //http  服务器
 
 //需要请求数据  本地/服务器  发送AJAX 参数: url; 数据; 成功的函数; 失败的函数
-function localAjax(url, content, success, error) { 
-	
-	var local_url = url_head + url;
+function setAjax(url, content, success, error) { 	
+	var set_url = url_head + url;
 	$.ajax({
-		url: local_url,
+		url: set_url,
 		data: JSON.stringify(content),
 		type: "post",
 		dataType: "json",
@@ -17,10 +16,10 @@ function localAjax(url, content, success, error) {
 }
 
 //不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
-function localAjaxN(url, success, error) {
-	var local_url = url_head + url;
+function setAjaxN(url, success, error) {
+	var set_url = url_head + url;
 	$.ajax({
-		url: local_url,
+		url: set_url,
 		type: "post",
 		dataType: "json",
 		success,
