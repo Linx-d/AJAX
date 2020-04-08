672a2c8c0f66fc5c8e46bbe36651d82b6412ad70
lixin <q1404433837@163.com>
2020-04-08 10:00:55 +0800

    [feat] 未分类

---
+++ REDME.md
@@ -0,0 +1,23 @@
+### …or create a new repository on the command line
+
+
+
+```
+echo "# AJAX" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:Linx-d/AJAX.git
+git push -u origin master
+                
+```
+
+### …or push an existing repository from the command line
+
+
+
+```
+git remote add origin git@github.com:Linx-d/AJAX.git
+git push -u origin master
+```
+
---
+++ ajaxMy.js
@@ -0,0 +1,29 @@
+var url_head = "http://192.168.0.180:8081/api"; //http 本地
+//var url_head = "http://127.0.0.1:8888/api"; //http  服务器
+
+//需要请求数据  本地/服务器  发送AJAX 参数: url; 数据; 成功的函数; 失败的函数
+function localAjax(url, content, success, error) { 
+	
+	var local_url = url_head + url;
+	$.ajax({
+		url: local_url,
+		data: JSON.stringify(content),
+		type: "post",
+		dataType: "json",
+		contentType: "application/json",
+		success,
+		error
+	});
+}
+
+//不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
+function localAjaxN(url, success, error) {
+	var local_url = url_head + url;
+	$.ajax({
+		url: local_url,
+		type: "post",
+		dataType: "json",
+		success,
+		error
+	});
+}
