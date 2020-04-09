050e41043d89bf8db876db7f9ad2ea4e32f4c09c
lixin <q1404433837@163.com>
2020-04-09 09:20:14 +0800

    [feat] 新增同步ajax请求

--- ajaxMy.js
+++ ajaxMy.js
@@ -1,7 +1,7 @@
 var url_head = "http://192.168.0.180:8081/api"; //http 本地
 //var url_head = "http://127.0.0.1:8888/api"; //http  服务器
 
-//需要请求数据  本地/服务器  发送AJAX 参数: url; 数据; 成功的函数; 失败的函数
+//异步 需要请求数据  本地/服务器  发送AJAX 参数: url; 数据; 成功的函数; 失败的函数
 function setAjax(url, content, success, error) { 	
 	var set_url = url_head + url;
 	$.ajax({
@@ -15,10 +15,38 @@ function setAjax(url, content, success, error) {
 	});
 }
 
-//不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
+// 异步 不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
 function setAjaxN(url, success, error) {
 	var set_url = url_head + url;
 	$.ajax({
+		url: set_url,
+		type: "post",
+		dataType: "json",
+		success,
+		error
+	});
+}
+
+// 同步 不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
+function setAjaxA(url, content, success, error) { 	
+	var set_url = url_head + url;
+	$.ajax({
+		async: false,
+		url: set_url,
+		data: JSON.stringify(content),
+		type: "post",
+		dataType: "json",
+		contentType: "application/json",
+		success,
+		error
+	});
+}
+
+// 同步 不需要请求数据  本地/服务器  发送AJAX 参数: url; 成功的函数; 失败的函数
+function setAjaxNA(url, success, error) {
+	var set_url = url_head + url;
+	$.ajax({
+		async: false,
 		url: set_url,
 		type: "post",
 		dataType: "json",
